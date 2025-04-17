5d78a58ef21f9e220bdcee29db583dc16dc040c7
Salmon <alex20000521@gmail.com>
2025-04-18 01:26:46 +0800

    [Test] Improve code coverage

--- src/test/java/dao/product/ProductDAOImplTest.java
+++ src/test/java/dao/product/ProductDAOImplTest.java
@@ -5,6 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import util.DatabaseUtil;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class ProductDAOImplTest {
     private ProductDAO productDAO;
 
@@ -15,14 +19,31 @@ public class ProductDAOImplTest {
     }
 
     @Test
-    public void getID() {
+    public void test_getProductsByType() {
+        List<Product> productList = productDAO.getProductsByType("Boutique hand coffee");
+        assertEquals(7, productList.size());
+        productList = productDAO.getProductsByType("Chocolate");
+        assertEquals(5, productList.size());
+        productList = productDAO.getProductsByType("Tea");
+        assertEquals(5, productList.size());
+        productList = productDAO.getProductsByType("Milk");
+        assertEquals(5, productList.size());
+        productList = productDAO.getProductsByType("Coffee");
+        assertEquals(7, productList.size());
+        productList = productDAO.getProductsByType("Latte");
+        assertEquals(7, productList.size());
+    }
+
+    @Test
+    public void test_getProductID() {
         Product product = new Product();
         product.setName("耶加雪非");
         product.setIce("熱");
         product.setSize("M");
         product.setSugar("無糖");
         product.setPrice(70);
+
         Integer ProductID = productDAO.getProductID(product);
-        System.out.println(ProductID);
+        assertEquals(2, ProductID.intValue());
     }
 }
